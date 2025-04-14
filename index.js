5ebc9dc6a0feeb05897cb81e56e140080768e82a
Alexander Ariza <66517893+Alexanderariza@users.noreply.github.com>
2025-04-14 23:31:13 +0200

    Update index.js

--- index.js
+++ index.js
@@ -25,10 +25,22 @@ const storage = multer.diskStorage({
   }
 });
 
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/');
+  },
+  filename: (req, file, cb) => {
+    const ext = path.extname(file.originalname);
+    const uniqueName = Date.now() + '-' + Math.round(Math.random() * 1E9) + ext;
+    cb(null, uniqueName);
+  }
+});
+
 const upload = multer({
   storage,
   limits: { fileSize: 500 * 1024 } // 500 KB
 });
+
 
 
 const pool = new Pool({
